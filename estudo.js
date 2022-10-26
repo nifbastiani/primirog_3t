be4a547a17b4b5bffea8ded979580cd271020a10
nifbastiani <108945028+nifbastiani@users.noreply.github.com>
2022-10-26 11:55:10 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -2,6 +2,28 @@ function total(){
     let c = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
+
+    if(!Number(c)){
+      alert(" o valor do capital deve ser um número.");
+      document.getElementById("capital").value = "";
+      document.getElementById("capital");
+      return 
+    }
+
+    if(!Number(j)){
+      alert(" o valor dos juros deve ser um número.");
+      document.getElementById("juros").value = "";
+      document.getElementById("juros").focus ();
+      return 
+    }
+
+    if(!Number(m)){
+      alert(" o valor dos meses deve ser um número.");
+      document.getElementById("meses").value = "";
+      document.getElementById("meses").focus ();
+      return 
+    }
+
     let r = 0;
     for(let i = 1; i <= t; i++){
         r = c * (1 + (j/100));
